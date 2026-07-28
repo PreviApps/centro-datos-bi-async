3f186811445f227d4ffb829c0a3bb6c36d44d698
Sergio <sergio011304@gmail.com>
2026-07-28 16:41:44 -0500

    Ajuste de URL destino frontend-backend

--- frontend/src/environments/URL.jsx
+++ frontend/src/environments/URL.jsx
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://localhost:3008/reports"
-//export const BASE_URL = "http://10.10.119.45:7070/reports"
+//export const BASE_URL = "http://localhost:3008/reports"
+export const BASE_URL = "http://10.10.119.45:3008/reports"
 //export const BASE_URL = "http://192.168.2.74:8080/reports"
 //export const BASE_URL = "http://192.168.1.24:8080/reports"
