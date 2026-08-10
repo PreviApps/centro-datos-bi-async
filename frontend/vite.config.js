0f7b83c9f53c53cfc53cca40431068a9eed711f5
Sergio <sergio011304@gmail.com>
2026-08-10 15:35:33 -0500

    Agregando los hosts por los cuales se accederá a la aplicación

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,5 +6,16 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [
     tailwindcss(),
-    react()]
+    react()],
+  server: {
+    host: '0.0.0.0', // Necesario para Docker
+    port: 4008,
+    allowedHosts: [
+      'localhost',
+      '127.0.0.1',
+      '10.10.119.45',
+      '10.10.119.97',
+      'centro-datos-bi-v2.previsalud.com.co'
+    ]
+  }
 })
