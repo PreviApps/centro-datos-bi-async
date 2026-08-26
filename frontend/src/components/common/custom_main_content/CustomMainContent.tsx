5353312466d90d70c9b76898af3ad8411a94af91
Sergio <sergio011304@gmail.com>
2026-08-26 11:23:52 -0500

    Ajustando estilos para tamaño del vison del tablero

--- frontend/src/components/common/custom_main_content/CustomMainContent.tsx
+++ frontend/src/components/common/custom_main_content/CustomMainContent.tsx
@@ -6,7 +6,7 @@ interface MainLayoutProps {
 
 const CustomMainContent: React.FC<MainLayoutProps> = ({ children }) => {
   return (
-    <div className="min-h-screen w-full bg-[#f3f4f6] text-gray-800 font-sans antialiased">
+    <div className="flex flex-col h-screen w-full bg-[#f3f4f6] text-gray-800 font-sans antialiased overflow-hidden">
       <main className="container mx-auto px-4 py-6 md:py-4">
         {children}
       </main>
